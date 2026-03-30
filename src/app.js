1bd958afc23f4c42d7e88509b437d116201c8f07
Chiranjeevi Gurram <chiranjeevigurram@Chiranjeevis-MacBook-Pro.local>
2026-03-30 10:12:35 +0530

    [#1] | <adds logger in app.js> | [<chiru>/<sirisha>]

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 import { Hono } from "hono";
 import { serveStatic } from "hono/deno";
+import { logger } from "hono/logger";
 
 export const createApp = () => {
   const app = new Hono();
-
+  app.use(logger());
   app.use(serveStatic({ root: "public" }));
 
   return app;
