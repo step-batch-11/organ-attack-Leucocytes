ee7ef9d7b8f2a6dbc0db9d04c7d829eedc560776
Vivek <230504152+geekyvivekbytes@users.noreply.github.com>
2026-03-30 09:57:44 +0530

    Adds git hooks setup using symlinks and executable permissions

--- setup.sh
+++ setup.sh
@@ -1,4 +1,14 @@
 #! /bin/bash
-cp hooks/* .git/hooks/
 
-chmod u+x .git/hooks/pre-commit .git/hooks/pre-push
+chmod u+x ./hooks/pre-commit
+chmod u+x ./hooks/pre-push
+
+echo "Execute permission set"
+
+ln -s ../../hooks/pre-commit .git/hooks/pre-commit
+ln -s ../../hooks/pre-push .git/hooks/pre-push
+
+echo "Sym links created no need to copy hooks in the '.git/hooks/' folder "
+
+touch .env 
+
