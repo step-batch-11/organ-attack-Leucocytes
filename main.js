a5f993055b89f26d3915288c30ac677660c5e3e8
Chiranjeevi Gurram <chiranjeevigurram@Chiranjeevis-MacBook-Pro.local>
2026-03-30 12:27:04 +0530

    [#1] | implements login functionality | [chiru/sirisha]

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 import { createApp } from "./src/app.js";
 
+export const counter = () => {
+  let i = 0;
+  return () => ++i;
+};
+
 const main = () => {
-  const app = createApp();
+  const session = {};
+  const idGenerator = counter();
+
+  const app = createApp({ session, idGenerator });
   const port = Deno.env.get("PORT") || 8000;
 
   Deno.serve({ port }, app.fetch);
