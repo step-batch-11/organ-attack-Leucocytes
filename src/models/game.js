39361e2f58a622e4d0967622004cb7acea0561c1
Nikhil Chodavarapu <nikhilchodavarapu@Nikhils-MacBook-Pro.local>
2026-03-30 13:02:16 +0530

    [#4] | Moves card distribution logic into Game model and removes standalone distributor module | [Nikhil/Vivek]

--- src/models/game.js
+++ src/models/game.js
@@ -1,7 +1,34 @@
 export class Game {
   #players;
-  constructor(players) {
+  #attackCards;
+  #discardPile;
+  #organCards;
+
+  constructor(players, attackCards, organCards) {
     this.#players = players;
+    this.#attackCards = attackCards;
+    this.#organCards = organCards;
+    this.#discardPile = [];
+  }
+
+  #shuffleCards(cardSuffler) {
+    this.#attackCards = cardSuffler(this.#attackCards);
+    this.#organCards = cardSuffler(this.#organCards);
+  }
+
+  distributeCards(cardSuffler) {
+    this.#shuffleCards(cardSuffler);
+
+    const organCardsLimit = Math.floor(
+      this.#organCards.length / this.#players.length,
+    );
+    const attackCardsLimit = 5;
+
+    this.#players.forEach((player) => {
+      const playerAttackCards = this.#attackCards.splice(0, attackCardsLimit);
+      const playerOrganCards = this.#organCards.splice(0, organCardsLimit);
+      player.fillHand(playerAttackCards, playerOrganCards);
+    });
   }
 
   getPlayers() {
--- test/game_test.js
+++ test/game_test.js
@@ -1,6 +1,7 @@
 import { describe, it } from "@std/testing/bdd";
 import { assertEquals, assertInstanceOf } from "@std/assert";
 import { Game } from "../src/models/game.js";
+import { Player } from "../src/models/player.js";
 
 describe("Game model test", () => {
   describe("* GetPlayers test", () => {
@@ -11,6 +12,50 @@ describe("Game model test", () => {
 
       assertInstanceOf(game, Game);
       assertEquals(playerDetails, ["Player"]);
+    });
+  });
+
+  describe("Testing Cards Distributor", () => {
+    it("Should distribute 5 attack cards and 4 organ cards for 2 players", () => {
+      const [player1, player2] = [
+        new Player("Shivang", 1),
+        new Player("Samiran", 1),
+      ];
+
+      const attackCards = Array.from({ length: 10 }, (_, i) => `a${i + 1}`);
+      const organCards = Array.from({ length: 8 }, (_, i) => `o${i + 1}`);
+
+      const game = new Game([player1, player2], attackCards, organCards);
+
+      game.distributeCards((deck) => deck);
+
+      const { attackCards: player1Attacks, organCards: player1Organs } = player1
+        .getPlayerDetails();
+      assertEquals(player1Attacks, ["a1", "a2", "a3", "a4", "a5"]);
+      assertEquals(player1Organs, ["o1", "o2", "o3", "o4"]);
+
+      const { attackCards: player2Attacks, organCards: player2Organs } = player2
+        .getPlayerDetails();
+      assertEquals(player2Attacks, ["a6", "a7", "a8", "a9", "a10"]);
+      assertEquals(player2Organs, ["o5", "o6", "o7", "o8"]);
+    });
+
+    it("Should distribute cards for 6 players", () => {
+      const players = Array.from(
+        { length: 6 },
+        (_, i) => new Player(`p${i + 1}`, i + 1),
+      );
+      const attackCardsDeck = Array.from({ length: 40 }, (_, i) => `a${i}`);
+      const organCardsDeck = Array.from({ length: 24 }, (_, i) => `o${i}`);
+
+      const game = new Game(players, [...attackCardsDeck], [...organCardsDeck]);
+      game.distributeCards((deck) => deck);
+
+      players.forEach((player) => {
+        const { attackCards, organCards } = player.getPlayerDetails();
+        assertEquals(attackCards, attackCardsDeck.splice(0, 5));
+        assertEquals(organCards, organCardsDeck.splice(0, 4));
+      });
     });
   });
 });
