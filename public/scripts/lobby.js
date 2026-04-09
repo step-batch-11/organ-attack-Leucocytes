626ba5ba82f2ba27caa048b71a88c3533642f92d
Sirisha Dalasari <sirishadalasari2006@gmail.com>
2026-04-09 14:46:27 +0530

    [#43] | shows host start button only when more than one player is in the lobby | [siri]

--- public/scripts/lobby.js
+++ public/scripts/lobby.js
@@ -17,32 +17,39 @@ const startGame = async () => {
   window.location.href = "/game-page";
 };
 
-const renderTableFooter = (initLobbyIntervalID, roomID) => {
+const renderTableFooter = (initLobbyIntervalID, roomID, currentPlayersCount) => {
   const tableFooter = document.querySelector("#table-footer");
   const button = document.createElement("button");
 
-  tableFooter.innerHTML = "";
-  button.textContent = "Start";
-  button.classList.add("start-button");
-  tableFooter.append(button);
+    if (currentPlayersCount > 1) {
+    tableFooter.innerHTML = "";
+    button.textContent = "Start";
+    button.classList.add("start-button");
+    tableFooter.append(button);
 
-  button.addEventListener("click", () => {
-    clearInterval(initLobbyIntervalID);
-    triggerGameSetup(roomID);
-    window.location.href = "/game-page";
-  });
+    button.addEventListener("click", () => {
+      clearInterval(initLobbyIntervalID);
+      triggerGameSetup(roomID);
+      window.location.href = "/game-page";
+    });
+    return;
+    
+  }
+
+  const waitingMsg = document.querySelector("#waiting-msg");
+  waitingMsg.textContent = "waiting for players to join"
 };
 
 (() => {
   let initLobbyIntervalID;
 
   const initiateLobby = async () => {
-    const response = await fetch("/get-players").catch(() => {});
+    const response = await fetch("/get-players").catch(() => { });
     const { players, myID, roomID, redirectPath } = await response.json();
     console.log({ redirectPath });
 
     renderPlayers(players, myID, roomID);
-    if (amIHost(players, myID)) renderTableFooter(initLobbyIntervalID, roomID);
+    if (amIHost(players, myID)) renderTableFooter(initLobbyIntervalID, roomID, players.length);
   };
 
   window.onload = () => {
